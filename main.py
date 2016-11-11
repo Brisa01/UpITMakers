a987366aec514c46f80954fc356e88fef5781284
Brisa01 <brisa.se.re.mo@gmail.com>
2016-11-11 14:02:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import atexit
+import dweepy
 import ConfigParser
 import signal
 import sys
@@ -10,16 +11,22 @@ import pyupm_grove as grove
 import pyupm_grovespeaker as upmGrovespeaker
 import pyupm_i2clcd as lcd
 
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 credentials = ConfigParser.ConfigParser()
 credentialsfile = "credentials.config"
 credentials.read(credentialsfile)
 
+
 button = grove.GroveButton(8)
 display = lcd.Jhd1313m1(0, 0x3E, 0x62)
 light = grove.GroveLight(0)
+temp=grove.GroveTemperature(1)
 relay = grove.GroveRelay(2)
+
+datafreeboard = {}
+datadweet = "ThisIsDweeting"
 
 def functionLight(bot, update):
     luxes = light.value()
@@ -27,9 +34,6 @@ def functionLight(bot, update):
 
 def functionMessage(bot, update):
     bot.sendMessage(update.message.chat_id, text=message)
-
-def functionPicture(bot, update):
-    bot.sendPhoto(update.message.chat_id, photo=open('documentation/image.jpg', 'rb'))
 
 def functionRelay(bot, update):
     relay.on()
@@ -40,11 +44,20 @@ def functionRelay(bot, update):
 def functionEcho(bot, update):
     bot.sendMessage(update.message.chat_id, text=update.message.text)
 
+def functionTemperature(bot, update):
+    tempe=temp.value()
+    bot.sendMessage(update.message.chat_id, text='Temperature! ' + str(tempe))
+
 def SIGINTHandler(signum, frame):
 	raise SystemExit
-
 def exitHandler():
 	print "Exiting"
+        time.sleep(2)
+        datafreeboard['alive'] = "0"
+        datafreeboard['luxes'] =  0
+        datafreeboard['tempe'] =  0
+        datafreeboard['message'] = "None"
+        dweepy.dweet_for(datadweet, datafreeboard)
 	sys.exit(0)
 
 atexit.register(exitHandler)
@@ -52,19 +65,20 @@ signal.signal(signal.SIGINT, SIGINTHandler)
 
 if __name__ == '__main__':
 
+
     credential = credentials.get("telegram", "token")
     updater = Updater(credential)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("light", functionLight))
+    dp.add_handler(CommandHandler("temp", functionTemperature)) 
     dp.add_handler(CommandHandler("message", functionMessage))
-    dp.add_handler(CommandHandler("picture", functionPicture))
     dp.add_handler(CommandHandler("relay", functionRelay))
     dp.add_handler(MessageHandler([Filters.text], functionEcho))
 
     updater.start_polling()
 
-    message = "Dzibilchaltun"
+    message = "ITMakers"
 
     while True:
 
@@ -72,6 +86,12 @@ if __name__ == '__main__':
         luxes = int(luxes)    
         display.setColor(luxes, luxes, luxes)
         display.clear()
+        
+        datafreeboard['alive'] = "1"
+        datafreeboard['luxes'] =  luxes
+        datafreeboard['tempe'] =  tempe
+        datafreeboard['message'] = message
+        dweepy.dweet_for(datadweet, datafreeboard)
 
         if button.value() is 1:
             display.setColor(255, 255, 0)
